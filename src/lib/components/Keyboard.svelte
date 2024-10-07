2aaa0b2708154a72bd88c75a56d736d0cfdeff7b
ErikKL06 <06kler@skola.engelholm.se>
2024-10-07 09:56:03 +0200

    second commit

---
+++ src/lib/components/Keyboard.svelte
@@ -0,0 +1,21 @@
+<section id = 'keyBoard'>
+    <button id='b7' on:click>7</button>
+    <button id='b8' on:click>8</button>
+    <button id='b9' on:click>9</button>
+    <button id='add' on:click>+</button>
+    <button id='b4' on:click>4</button>
+    <button id='b5' on:click>5</button>
+    <button id='b6' on:click>6</button>
+    <button id='sub' on:click>-</button>
+    <button id='b1' on:click>1</button>
+    <button id='b2' on:click>2</button>
+    <button id='b3' on:click>3</button>
+    <button id='mul' on:click>x</button>
+    
+    <button id='comma' on:click>,</button>
+    <button id='b0' on:click>0</button>
+    <button id='enter' on:click>=</button>
+    <button id='div' on:click>/</button>
+    <button id='clear' style = 'grid-column: span 4;' on:click>CLEAR</button>
+</section>
+
